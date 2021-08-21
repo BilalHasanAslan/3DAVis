acfbf18d15c681a748a484460ce448e317a29a8c
Bilal Hasan Aslan <bilalhasanaslan@gmail.com>
2021-08-21 23:22:11 +0200

    Converter from fits to hdf5

---
+++ Server/Converter/ConverterAverage/MipMapUtil.h
@@ -0,0 +1,19 @@
+#ifndef MIPMAPUTIL_H_
+#define MIPMAPUTIL_H_
+
+#include <H5Cpp.h>
+#include <fitsio.h>
+#include <vector>
+namespace NDAVis
+{
+
+    struct MipMapUtil
+    {
+        std::vector<hsize_t> mipDims(const std::vector<hsize_t> &dims, int mip, int zmip);
+        bool useChunks(const std::vector<hsize_t> &dims);
+        void createHdf5Dataset(H5::DataSet& dataset, H5::Group group, std::string path, H5::DataType dataType, std::vector<hsize_t> dims, const std::vector<hsize_t>& chunkDims = std::vector<hsize_t>());
+        void readFitsData(fitsfile *filePtr, hsize_t channel, unsigned int stokes, hsize_t size, float *destination);
+    };
+}
+
+#endif //MIPMAPUTIL_H_
---
+++ Server/Converter/ConverterMax/Converter.h
@@ -0,0 +1,39 @@
+#ifndef CONVERTER_H_
+#define CONVERTER_H_
+
+#include <H5Cpp.h>
+#include <fitsio.h>
+#include <string>
+#include <vector>
+#include "MipMap.h"
+
+namespace NDAVis
+{
+    class Converter
+    {
+    public:
+        Converter(std::string inputFileName, std::string outputFileName);
+        void openFitsFile(fitsfile **filePtrPtr, std::string inputFileName);
+        fitsfile *inputFilePtr;
+        long dims[4];
+        int N;
+        void getFitsDims(fitsfile *filePtr, int &N, long *dims);
+        hsize_t depth, height, width;
+        std::vector<hsize_t> standardDims;
+        std::vector<hsize_t> tileDims;
+        MipMaps mipMaps;
+        void convert();
+        void copyAndCalculate();
+        float *standardCube;
+        std::string tempOutputFileName;
+        std::string outputFileName;
+            H5::DataSet standardDataSet;
+
+        // Main HDF5 objects
+        H5::H5File outputFile;
+        H5::Group outputGroup;
+    };
+
+}
+
+#endif //CONVERTER_H_
